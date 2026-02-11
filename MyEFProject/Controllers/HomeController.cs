a486cd0ca4588b69cbeabd90da1d3d2b39e89714
awmxr <amir.m.n.1380@gmail.com>
2026-02-11 11:15:20 +0330

    BulkOperations | AddRange | Remove Range

--- MyEFProject/Controllers/HomeController.cs
+++ MyEFProject/Controllers/HomeController.cs
@@ -25,12 +25,12 @@ public class HomeController : Controller
     public IActionResult Upsert(int? id)
     {
         Category category = new();
-        if(id == null)
+        if (id == null)
         {
             return View(category);
         }
         category = _db.Categories.FirstOrDefault(c => c.Id == id);
-        if(category == null)
+        if (category == null)
         {
             return NotFound();
         }
@@ -42,7 +42,7 @@ public class HomeController : Controller
     [HttpPost]
     public IActionResult Upsert(Category category)
     {
-        if(category.Id == 0)
+        if (category.Id == 0)
         {
             _db.Categories.Add(category);
         }
@@ -62,6 +62,55 @@ public class HomeController : Controller
         _db.Categories.Remove(category);
         _db.SaveChanges();
         return RedirectToAction("Index");
+
+
+    }
+
+    public IActionResult AddMultipleRecords()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            _db.Categories.Add(
+                new Category()
+                {
+                    Name = "Category " + i
+
+                }
+                );
+            
+        }
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+
+
+    }
+
+    public IActionResult AddMultipleRecords2()
+    {
+        List<Category> categories = new List<Category>();   
+        for (int i = 20; i < 30; i++)
+        {
+            categories.Add(new Category()
+            {
+                Name = $"Cat {i}"
+            });
+
+        }
+
+        _db.Categories.AddRange(categories);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+
+
+    }
+
+    public IActionResult RemoveRange()
+    {
+        List<Category> categories = _db.Categories.OrderByDescending(c => c.Id).Take(5).ToList();
+        _db.Categories.RemoveRange(categories);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+
 
 
     }
