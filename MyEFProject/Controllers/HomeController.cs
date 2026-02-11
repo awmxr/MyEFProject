2a6bba92b4239d6a8e1a050aeff6c612e97b8800
awmxr <amir.m.n.1380@gmail.com>
2026-02-11 11:04:43 +0330

    Upsert and delete

--- MyEFProject/Controllers/HomeController.cs
+++ MyEFProject/Controllers/HomeController.cs
@@ -22,5 +22,49 @@ public class HomeController : Controller
         return View(list);
     }
 
-    
+    public IActionResult Upsert(int? id)
+    {
+        Category category = new();
+        if(id == null)
+        {
+            return View(category);
+        }
+        category = _db.Categories.FirstOrDefault(c => c.Id == id);
+        if(category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+
+    }
+
+    [HttpPost]
+    public IActionResult Upsert(Category category)
+    {
+        if(category.Id == 0)
+        {
+            _db.Categories.Add(category);
+        }
+        else
+        {
+            _db.Categories.Update(category);
+        }
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+
+    public IActionResult Delete(int id)
+    {
+        var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+        _db.Categories.Remove(category);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+
+
+    }
+
+
 }
